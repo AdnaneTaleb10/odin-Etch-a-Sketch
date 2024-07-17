75cf1b0e1352093f1167f4132fb9310499f0bfcd
Adnane Taleb <adnane102464@gmail.com>
2024-07-17 23:23:50 +0100

    Update createGrid function and add square control with range input

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-function drowSquaresOfGrid(){
-    const grid = document.querySelector('.grid-container');
-    let square = document.createElement('div');
-    square.setAttribute('class' , 'grid-item');
-    square.style.width = '4%';
-    square.style.height = '4%';
-    grid.appendChild(square);
+function createGrid (gridSize){
+    let itemWidth = 100 / gridSize;
+    let itemHeight = 100 / gridSize;
+    let grid = document.querySelector('.grid-container');
+
+    for(let i = 0 ; i < gridSize * gridSize ; i++){
+        let square = document.createElement('div');
+        square.setAttribute('class' , 'grid-item');
+        square.style.width = `${itemWidth}%`;
+        square.style.height = `${itemHeight}%`;
+        grid.appendChild(square);
+    }   
 }
 
-for(let i = 0 ; i < 625 ; i++){
-    drowSquaresOfGrid()
+
+
+function slider(){
+    let height = document.querySelector('.gridHeight');
+    let width = document.querySelector('.gridWidth');
+    let sliderValue = document.querySelector('.slider');
+    height.textContent = `${sliderValue.getAttribute("value")}`;
+    width.textContent = `${sliderValue.getAttribute("value")}`;
+    sliderValue.addEventListener("input" , (e) => {
+        height.textContent = `${e.target.value}`;
+        width.textContent = `${e.target.value}`; 
+        let grid = document.querySelector('.grid-container');
+        grid.textContent = "";
+        createGrid(e.target.value)
+    } )   
 }
+
+createGrid(25);
+slider()
+
+
+
 
 
